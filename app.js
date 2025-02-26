edc3afd0e262139c7589af8681954edd94e79419
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-02-26 23:21:29 +0500

    update

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-let display = document.getElementById("inputBox");
-
-let buttons = document.querySelectorAll("button");
-
-let buttonsArray = Array.from(buttons);
-
-let string = "";
-
-buttonsArray.forEach(btn => {
-
-btn.addEventListener("click" , (e) =>{
-
-   if(e.target.innerHTML == "DEL"){
-string = string.substring(0, string.length-1);
-display.value =string;
-
-}else if(e.target.innerHTML == "AC"){
-    string ="";
-    display.value =string;
-}else if(e.target.innerHTML == "="){
-string =eval (string);
-display.value =string;
-}else{string +=e.target.innerHTML;
-    display.value = string;
-}
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
 
-});
+let inputString = '';
 
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.innerText;
+
+
+        if (value === 'AC') {
+            inputString = '';
+            display.value = inputString;
+        }
+
+        else if (value === 'DEL') {
+            inputString = inputString.slice(0, -1);
+            display.value = inputString;
+        }
+
+        else if (value === '=') {
+            try {
+
+                if (inputString.includes('%')) {
+                    inputString = inputString.replace(/([0-9]+)%/, '($1/100)');
+                }
+
+                inputString = Function('return ' + inputString)();
+                display.value = inputString;
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+
+        else {
+            inputString += value;
+            display.value = inputString;
+        }
+    });
 });
